a5f56899ebbca4e63800865c1f7a672a3d64999e
skeletorfw <francis.windram@hotmail.co.uk>
2017-02-10 03:10:40 +0000

    Created Run class and added items & trinkets datafiles.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# SCG
-Challenge XML Generator for Binding of Isaac Afterbirth+
+# Skeletorfw's Challenge Generator
+SCG is a challenge XML generator for Binding of Isaac Afterbirth+.
+
+It aims to provide an easy way to create valid challenge XML files using a simple interface or pasteable seed.
+
+### Notes
+#### IDs
+The item IDs were stripped from the HTML source of the PlatinumGod ( <3 to elucidator). To do this, a mixture of bash scripting, text editor magic, and eventually excel were all used.
+
+1. Firstly the source HTML was copied into a text file and split into segments for Items & Trinkets.
+2. Next this text file was de-crudded to return only lines containing the ID or name, using bash:
+
+```egrep 'item-title|TrinketID' PG_Dump_Items.txt >> Items_Strip1.txt```
+3. This file was then opened using a text editor. The html tags were stripped out and a comma was added between the pairs.
+4. Next the Item IDs were shifted to the beginning of the line using awk:
+
+```awk -F',' '{ print $2","$1 }' Items_Strip1.txt >> Items_Strip2.txt```
+5. And finally the list was sorted by item ID to make everything logical for import later.
+
+(Eventually I will possibly store all this data in a sqlite database to keep it all compact)
+
+### Thanks
+A great thanks to the following people for creating the resources which has made this possible:
+* DD [The Dignitary] - for creating the original XML challenge guide I followed to write this tool.
+* elucidator - for the fantastic PlatinumGod.com site.
+
+### Links
+
+* [Platinum God](http://platinumgod.co.uk/) - Information on all the mechanics and items in the game.
+* [How to create a custom challenge - Afterbirth+](http://steamcommunity.com/sharedfiles/filedetails/?id=835061601) - The original challenge XML guide.
--- SCG.py
+++ SCG.py
@@ -1,62 +1,54 @@
-# Skeletorfw's Challenge Generator v 0.0.1
-# Skeletorfw 2016
+# Skeletorfw's Challenge Generator v 0.0.2
+# Skeletorfw 2017
 # Python 3.4
 #
 # Created:  08/02/17
-# Modified: 08/02/17
+# Modified: 09/02/17
 #
-# A Python3/SQL project for building and maintaining a book database.
+# A Challenge XML Generator for Binding of Isaac Afterbirth+.
 #
 # ===== TO DO LIST =====
 # TODO: Place cases into dicts
-# TODO: Create nice class for run
+# DONE: Create nice class for run
 # TODO: Output general class to XML file
 # TODO: Create easy way to share runs: Seed system
-
-# VARS
-# Ranges are inclusive for now
-# id = 0
-# name = ""
-# endstage = 0    # between 6 - 12
-# items = []
-# trinkets = []
-# pill = 0        # between -1 - 46
-# card = 0        # between -1 - 51
-# playertype = 0  # between 0 - 15
-# bannedrooms = []    # between 1 - 22
-# bannedcurses = []   # 1, 2, 4, 8 or 16
-# forcedcurse = 0     # 1, 2, 4, 8 or 16
-# achievements = []
-# forcepath = 0   # -1 to +1, where 0 does not force a path.
-# canshoot = True
-# redhp = 0   # 2 = 1 heart
-# maxhp = 0
-# soulhp = 0
-# blackhp = 0
-# coins = 0
-# maxdmg = 100    # cannot be less than 100
-# adddmg = 0
-# minfirerate = 0
-# minshotspeed = 0
-# bigrange = False
-# hardmode = False    # Sets difficulty to 1 if True
-# megasatan = False
-
-
-
+# TODO: Create gui using tkinter
 
 # SEEDS
-#
+# Could just cat all vars together as a CSF, separating list entries e.g. items with dots, then base64 encode?
+# 1,name,6,404.412.300.12,15,5,0,3,1.3,2.4.8,16,50.51,-1,1,0,0,0,0,3,100,0,0,0,0,0,1
 
 # import tkinter
 
+
 class Run:
     """Holds run data"""
-    def __init__(self, runid, runname, endstage, items, trinkets, title, language):
+    def __init__(self, runid, runname, endstage):
+        # Required variables
         self.runid = runid
         self.runname = runname
-        self.endstage = endstage
-        self.items = items
-        self.trinkets = trinkets
-        self.title = title
-        self.language = language
+        self.endstage = endstage    # between 6 - 12, 8+ requires "altpath = True"
+        # Optional run variables
+        self.items = []
+        self.trinkets = []
+        self.startpill = -1     # between -1 - 46   -1 = No Pill
+        self.startcard = 0      # between -1 - 51   -1 = Random
+        self.playertype = 0     # between 0 - 15
+        self.bannedrooms = []   # between 1 - 22
+        self.bannedcurses = []  # 1, 2, 4, 8 or 16
+        self.forcedcurse = 0    # 1, 2, 4, 8 or 16
+        self.achievements = []
+        self.forcepath = 0      # -1 to +1, where 0 does not force a path. +1 sets altpath to be True
+        self.canshoot = True
+        self.redhp = 0   # 2 = 1 heart
+        self.maxhp = 0
+        self.soulhp = 0
+        self.blackhp = 0
+        self.coins = 0
+        self.maxdmg = 100    # cannot be less than 100
+        self.adddmg = 0
+        self.minfirerate = 0
+        self.minshotspeed = 0
+        self.bigrange = False
+        self.hardmode = False    # Sets difficulty to 1 if True
+        self.megasatan = False
